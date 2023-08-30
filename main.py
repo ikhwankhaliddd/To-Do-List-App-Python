2e12f3b80cf1f861889c0f6ec3310ce2a950b17f
Muhammad Ikhwan Khalid Nasution <muhammad.nasution@amartha.com>
2023-08-30 20:23:22 +0700

    nyelesaiin sisa fiturnya

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
-# 1. menambahkan aktivitas
-# 2. ngeliat semua aktivitas yang udah ditambahkan
-# 3. menghapus aktivitas
-# 4. tandain aktivitas kalo udah selesai
-
-# Clean code
-# 1. supaya orang baca nya mudah (readibility)
-# 2. biar ketauan error nya dimana (maintaiable)
-# 3. supaya kalo kita baca ulang itu ga bingung (readble & maintainable)
-
-tasks = []
+to_do_list = []
 
 def add_task():
   task = input("Task : ")
-  tasks.append(task)
+  to_do_list.append(task)
   print(f"Tugas {task} berhasil ditambah")
   
+def view_tasks():
+  if len(to_do_list) == 0:
+    print("Tidak ada task")
+  else:
+    print("Daftar Tugas : ")
+    for index, task in enumerate(to_do_list, start = 1):
+      print("{}.{}".format(index, task))
+      
+def delete_task():
+  view_tasks()
+  try:
+    task_number = int(input("Enter the number of the task"))
+    if 1 <= task_number <= len(to_do_list):
+      to_do_list.pop(task_number-1)
+      print("Task berhasil dihapus")
+  except ValueError:
+    print("Task Number Tidak Valid")
+    
+def mark_as_done():
+    view_tasks()
+    try:
+      task_number = int(input("Enter the number of the task"))
+      if 1 <= task_number <= len(to_do_list):
+        to_do_list.pop(task_number-1)
+        print("Task masrked as done")
+    except ValueError:
+      print("Task Number Tidak Valid")
+
 while True:
   print("TODO LIST APP")
   print("1. Add Task")
